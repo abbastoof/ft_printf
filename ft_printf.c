a75c9236b3dc5eda71ec9d22253742026b6c4f54
Abbas Toof <abbas.toof@gmail.com>
2022-12-09 00:01:39 +0200

    'last'

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 13:26:18 by atoof             #+#    #+#             */
-/*   Updated: 2022/12/08 19:19:02 by atoof            ###   ########.fr       */
+/*   Updated: 2022/12/08 21:16:15 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 typedef struct s_print
 {
 	va_list	arg;
-	int		width;
-	int		length;
-	int		total_length; //(return value) 
+	int		length; //(return value)
 	int		precision;
 	int		is_zero;
 	int		sign; //pos or neg
@@ -31,7 +29,6 @@ typedef struct s_print
 t_print	*ft_initialise(t_print *init)
 {
 	init->length = 0;
-	init->width = 0;
 	init->dash = 0;
 	init->is_zero = 0;
 	init->percentage = 0;
@@ -39,7 +36,6 @@ t_print	*ft_initialise(t_print *init)
 	init->precision = 0;
 	init->sign = 0;
 	init->space = 0;
-	init->total_length = 0;
 	init->zeropadding = 0;
 	return (init);
 }
@@ -47,25 +43,26 @@ t_print	*ft_initialise(t_print *init)
 int	ft_printf(const char *format, ...)
 {
 	t_print	*argument;
-	int		i;
 
 	argument = (t_print *)malloc(sizeof(t_print));
 	if (!argument)
 		return (-1);
 	ft_initialise(argument);
 	va_start(argument->arg, format);
-	i = -1;
-	while (*format)
+	while (*format) // string exists
 	{
-		if (format == '%')
-			ft_search_format(argument, format, i++);
+		if (format == '%') //if the char is %
+			format = ft_search_format(argument, *format++);
 		else
 			ft_putchar_fd(*format++, 1);
-		if (!format)
+		if (!format) 
 		{
-			ft_putstr_fd("(NULL)", 7);
+			ft_putstr_fd("(null)", 7);
 			va_end(argument->arg);
-			return (argument->length);	
+			free (argument);
+			return (argument->length);
 		}
 	}
+	va_end(argument->arg);
+	return (argument->length);
 }
