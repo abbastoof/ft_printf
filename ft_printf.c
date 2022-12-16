a00f5bc1e2938dd69b4fec62a1a2fee6a552bdb7
Abbas Toof <abbas.toof@gmail.com>
2022-12-16 23:07:32 +0200

    temp

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,66 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 13:26:18 by atoof             #+#    #+#             */
-/*   Updated: 2022/12/08 21:16:15 by atoof            ###   ########.fr       */
+/*   Updated: 2022/12/16 20:53:39 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-typedef struct s_print
+
+int	ft_search_format(t_print *args, const char *format)
 {
-	va_list	arg;
-	int		length; //(return value)
-	int		precision;
-	int		is_zero;
-	int		sign; //pos or neg
-	int		zeropadding; //"%02d"
-	int		point; //.
-	int		percentage; //%
-	int		dash; //-
-	int		space; //space flag ' ' 
-}			t_print;
+	int returnn;
+	int	index;
+
+	index = 0;
+	returnn = 0;
+	while (!"udcsupxX%")
+	{
+		if(format[index] == '.')
+			args->point = 1;
+			index++;
+		if(format[index] == '-')
+			args->dash = 1;
+			index++;
+		if(format[index] == ' ')
+			args->space = 1;
+			index++;
+		if(format[index] < 0)
+			args->sign = 1;
+		if(format[index] > 0)
+			args->sign = 1;
+	}
+}
+
+int	ft_check_format(t_print args, const char format)
+{
+	int	print_length;
+
+	print_length = 0;
+	if (format == 'c')
+		print_length += printchar(va_arg(args.arg, int));
+	else if (format == 's')
+		print_length += printstr(va_arg(args.arg, char *));
+	else if (format == 'p')
+		print_length += print_ptr(va_arg(args.arg, unsigned long long));
+	else if (format == 'd' || format == 'i')
+		print_length += printnbr(va_arg(args.arg, int));
+	else if (format == 'u')
+		print_length += print_unsigned(va_arg(args.arg, unsigned int));
+	else if (format == 'x' || format == 'X')
+		print_length += print_hex(va_arg(args.arg, unsigned int), format);
+	else if (format == '%')
+		print_length += printpercent();
+	return (print_length);
+}
 
 t_print	*ft_initialise(t_print *init)
 {
+	//set everything to false
 	init->length = 0;
 	init->dash = 0;
+	init->width = 0;
 	init->is_zero = 0;
 	init->percentage = 0;
 	init->point = 0;
@@ -42,27 +78,30 @@ t_print	*ft_initialise(t_print *init)
 
 int	ft_printf(const char *format, ...)
 {
-	t_print	*argument;
+	t_print	argument;
+	int		i;
+	int		printf_length;
 
-	argument = (t_print *)malloc(sizeof(t_print));
-	if (!argument)
+	argument.arg = (t_print *)malloc(sizeof(t_print));
+	if (!argument.arg)
 		return (-1);
-	ft_initialise(argument);
-	va_start(argument->arg, format);
-	while (*format) // string exists
+	ft_initialise(argument.arg);
+	va_start(argument.arg, format);
+	i = 0;
+	printf_length = 0;
+	while (format[i]) // string exists
 	{
-		if (format == '%') //if the char is %
-			format = ft_search_format(argument, *format++);
-		else
-			ft_putchar_fd(*format++, 1);
-		if (!format) 
+		if (format[i] == '%') //if the char is %
 		{
-			ft_putstr_fd("(null)", 7);
-			va_end(argument->arg);
-			free (argument);
-			return (argument->length);
+			printf_length += ft_check_format(argument, format[i + 1]);
+			i++;
 		}
+		else
+			printf_length += write(1, &format[i], 1);
+		i++;
 	}
-	va_end(argument->arg);
-	return (argument->length);
+	va_end(argument.arg);
+	printf_length += (argument.length);
+	free (argument);
+	return (printf_length);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 16:13:52 by atoof             #+#    #+#             */
-/*   Updated: 2022/12/07 16:16:54 by atoof            ###   ########.fr       */
+/*   Updated: 2022/12/16 02:22:20 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+
+typedef struct s_print
+{
+	va_list	arg;
+	int		length; //(return value)
+	int		precision;
+    int     width;
+	int		is_zero;
+	int		sign; //pos or neg
+	int		zeropadding; //"%02d"
+	int		point; //.
+	int		percentage; //%
+	int		dash; //-
+	int		space; //space flag ' ' 
+}			t_print;
 
 int		ft_printf(const char *format, ...);
 void	ft_putchar_fd(char c, int fd);
